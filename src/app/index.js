c0ee902ad4f68a780f6a359d5e4319d692c1c96f
Mikami91 <kaz7sense@hotmail.com>
2020-10-05 22:41:04 -0400

    Add Webpack and Babel

---
+++ src/app/index.js
@@ -0,0 +1,15 @@
+// Dependencies
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class App extends Component {
+    render() {
+        return (
+            <h1>
+                Okay
+            </h1>
+        );
+    }
+}
+
+render(<App />, document.getElementById('app'));
